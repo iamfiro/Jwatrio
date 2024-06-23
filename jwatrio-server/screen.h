609fa82fee2f9dea9d885aaf8e146229a90aca82
FIRO <appturbo102@gmail.com>
2024-06-23 15:10:52 +0900

    git restore

--- jwatrio-server/screen.h
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include <windows.h>
-#include <conio.h>
-#include <time.h>
-#include <stdlib.h>
-
-int screenStartNetwork(int screenX, int screenY);
--- jwatrio-server/text.h
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <windows.h>
-#include <conio.h>
-#include <time.h>
-#include <stdlib.h>
-
-void movePos(int x, int y);
-void setColor(int color);
